4f05040648804734bba9ee4082f9c2ddf09a60f5
Sagar Mandal <69377939+mandalsagar228@users.noreply.github.com>
2023-11-08 22:31:35 +0545

    db changes

--- server/Database/db.js
+++ server/Database/db.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
-const Connection = (username, password) => {
+const Connection = async (username, password) => {
   URL = `mongodb+srv://${username}:${password}@flipkartclone.ijk6zj8.mongodb.net/?retryWrites=true&w=majority`;
   try {
-    mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
+    await mongoose.connect(URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
     console.log("Database connected successfully");
   } catch (error) {
     console.log(
